eb4fd29099219dd50b3ea57cb44f655e09ec52b9
GeryMcGregor <gunamanta.yuana@gmail.com>
2024-01-03 15:47:51 +0800

    initial commit

---
+++ page/dashboard.php
@@ -0,0 +1,18 @@
+<?php 
+    require_once "template/header.php";
+    require_once "template/navbar.php";
+
+    switch ($_GET['page']) {
+        case 'value':
+            # code...
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+
+
+    require_once "template/footer.php";
+
+?>
