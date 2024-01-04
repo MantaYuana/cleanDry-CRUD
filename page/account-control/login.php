8bdf0da6e0a364f59ddd7b9c28cd5cdfa007b410
GeryMcGregor <gunamanta.yuana@gmail.com>
2024-01-04 23:27:55 +0800

    Created UI for Login Page

--- page/account-control/login.php
+++ page/account-control/login.php
@@ -1,21 +1,44 @@
-<section id="login">
+<section id="login" class="overflow-hidden">
 
-    <div class="container">
+    <div class="d-lg-flex flex-lg-row flex-column align-items-lg-center align-items-start" >
 
-        <form action="../php/helper/login_process.php" method="post">
-            <div class="mb-3">
-                <label for="login-username" class="form-label">Username</label>
-                <input type="text" name="login-user" class="form-control" id="login-username">
+        <div class="vh-100 d-flex align-items-center container-fluid col-lg-6 p-5" id="login-register-container" style="background-color: var(--mc-green-dark);">
+            <div class="login flex-grow-1">
+                <h4 class="text-light">CleanDry</h4>
+                <h2 class="text-light">Welcome Back !</h2>
+
+                <p class="text-light">Don't have an account yet ? <br> Then create one, it's free !</p>
+                <div class="d-grid gap-2">
+                    <a href="dashboard.php?page=register" class="btn btn-primary text-light">Register</a>
+                </div>
+
+                <form action="../php/helper/login_process.php" method="post">
+                    <div class="mb-3">
+                        <label for="login-username" class="form-label text-light">Username</label>
+                        <input type="text" name="login-user" class="form-control" id="login-username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="login-password" class="form-label text-light">Password</label>
+                        <input type="password" name="login-password" class="form-control" id="login-password">
+                        <div id="forgotPassword" class="form-text"><a href="#" class="link-light link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover">Forgot you password ?</a></div>
+                    </div>
+                    <!-- FIXME: make submit button stretch -->
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn btn-primary text-light">Submit</button>
+                    </div>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="login-password" class="form-label">Password</label>
-                <input type="password" name="login-password" class="form-control" id="login-password">
-                <div id="forgotPassword" class="form-text"><a href="#" class="link-primary link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Forgot you password ?</a></div>
-            </div>
-            <!-- FIXME: make submit button stretch -->
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
 
+        </div>
+
+        <div class="d-lg-inline d-none container col-6 p-5" id="cover" style="background-color: var(--mc-white-full);">
+            <div class="img">
+                <img class="img-fluid align-self-center" src="../assets/img/cover.png">
+            </div>
+            <div class="text-heading text-center">
+                <p>Quickly <b> clean all of your wardrobes </b>, using CleanDry latest <br>
+                    prepriortary <b> web based client application </b></p>
+            </div>
+        </div>
     </div>
-
 </section>
--- page/template/header.php
+++ page/template/header.php
@@ -9,7 +9,10 @@
     <!-- FIXME: favicon not working -->
     <link rel="icon" type="image/x-icon" href="../../assets/logo/favicon-dark.svg">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        @import url('../css/style.css');
+    </style>
+
 </head>
 
 <body>
